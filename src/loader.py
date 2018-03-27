8acd18728e410a48c05ea76df27628e228dc800f
estwanick <michaelestwanick@Michaels-MBP.fios-router.home>
2018-03-26 22:59:10 -0400

    adds data files

--- src/loader.py
+++ src/loader.py
@@ -2,12 +2,12 @@ import pandas as pd
 
 class DataFactory:
     def __init__(self, records=None):
-        self.predict = pd.read_csv('./data/zoo-animal-classification/class.csv', header=0, delimiter=',', dtype='category')
+        self.predict = pd.read_csv('./data/class.csv', header=0, delimiter=',', dtype='category')
 
         if records:
-            self.train = pd.read_csv('./data/zoo-animal-classification/zoo.csv', header=0, delimiter=',', nrows=records, dtype='category')
+            self.train = pd.read_csv('./data/zoo.csv', header=0, delimiter=',', nrows=records, dtype='category')
         else:
-            self.train = pd.read_csv('./data/zoo-animal-classification/zoo.csv', header=0, delimiter=',', dtype='category')
+            self.train = pd.read_csv('./data/zoo.csv', header=0, delimiter=',', dtype='category')
 
     def get_dataframe(self):
         return self.train
