530a87cf2f2694fe4e39a9b3af614cbb029a03f2
estwanick <michaelestwanick@Michaels-MBP.fios-router.home>
2018-03-26 22:49:25 -0400

    Refactor for Zoo Data set, implement k-fold cross validation

--- index.py
+++ index.py
@@ -1,12 +1,17 @@
 # Random Forest / Naiive Bayes
 from src.loader import DataFactory
+
+from src.DecisionTree import DecisionTree
 from src.RandomForest import RandomForest
-import matplotlib
 import numpy as np
 
 import matplotlib.pyplot as plt
 
-df = DataFactory(records=10)
-test_df, train_df = df.get_dataframe()
-rf = RandomForest(train_df, test_df)
-rf.run_simulation('rating')
+df = DataFactory()
+animal_df = df.get_dataframe()
+
+dt = DecisionTree(animal_df)
+dt.run_simulation('class_type')
+
+rf = RandomForest(animal_df)
+rf.run_simulation('class_type')
--- src/DecisionTree.py
+++ src/DecisionTree.py
@@ -1,26 +1,29 @@
-from sklearn.ensemble import RandomForestClassifier
+from sklearn import tree
 from sklearn.metrics import accuracy_score, cohen_kappa_score, confusion_matrix, classification_report
+import graphviz
 from sklearn.model_selection import train_test_split, cross_val_score
 
-class RandomForest:
+class DecisionTree:
     def __init__(self, train_df):
         self.train_df = train_df
 
     def run_simulation(self, predict_value):
-        print 'Random Forest: '
+        print 'Decision Tree: '
         X = self.train_df.drop(predict_value, axis=1).drop('animal_name', axis=1)
         y = self.train_df[predict_value]
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=27)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
 
-        random_forest = RandomForestClassifier(n_estimators=100, random_state=30)
-        random_forest.fit(X_train, y_train)
-        y_predict = random_forest.predict(X_test)
-        score = random_forest.score(X_test, y_test)
+        dt = tree.DecisionTreeClassifier()
+        dt.fit(X_train, y_train)
+        y_predicted = dt.predict(X_test)
+        score = dt.score(X_test, y_test)
 
         print '\t Model Accuracy: '
         print '\t', score
 
         # Cross validation
-        scores = cross_val_score(random_forest, X, y, scoring='accuracy', cv=10)
+        scores = cross_val_score(dt, X, y, scoring='accuracy', cv=10)
         print '\t Cross Validation: '
         print '\t', scores
+
+    
